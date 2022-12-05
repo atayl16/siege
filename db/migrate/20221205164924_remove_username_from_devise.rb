b603f0d2788d2e9c119a60b9ca4d890bbf7e9d73
Alisha Taylor <inspirebyexample0@gmail.com>
2022-12-05 17:51:01 +0100

    fix sign up bug

---
+++ db/migrate/20221205164924_remove_username_from_devise.rb
@@ -0,0 +1,5 @@
+class RemoveUsernameFromDevise < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :users, :username, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_05_154235) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_05_164924) do
   create_table "players", force: :cascade do |t|
     t.string "name"
     t.integer "xp"
@@ -24,14 +24,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_154235) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
