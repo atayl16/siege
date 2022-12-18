40ae6cc4d1dfe2f6a5b8370cc95f2f6206a1723c
Alisha Taylor <inspirebyexample0@gmail.com>
2022-12-18 20:25:25 +0100

    Do not update players from osrs if current xp is 0

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -79,12 +79,14 @@ class PlayersController < ApplicationController
 
   def call_osrs_api
     require 'httparty'
-    @url = HTTParty.get(
-      "https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=#{@player.name}",
-      :headers =>{'Content-Type' => 'application/json'}
-    )
-    @player.update( current_xp: @url.split("\n")[0].split(",").map(&:to_i).last )
-    @player.update( current_lvl: @url.split("\n")[0].split(",").map(&:to_i).second )
+        @url = HTTParty.get(
+          "https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=#{@player.name}",
+          :headers =>{'Content-Type' => 'application/json'}
+        )
+        if @url.split("\n")[0].split(",").map(&:to_i).last == 0 then return end
+        @player.update( current_xp: @url.split("\n")[0].split(",").map(&:to_i).last )
+        @player.update( current_lvl: @url.split("\n")[0].split(",").map(&:to_i).second )
+        puts "Updated #{@player.name}, current xp: #{@player.current_xp}, current lvl: #{@player.current_lvl}"
   end
 
   def add_member_to_wom
