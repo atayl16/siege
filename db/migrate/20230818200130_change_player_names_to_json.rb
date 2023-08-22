24fd438b2265b74db7b0446b6d8d93e0ee189047
Alisha Taylor <inspirebyexample0@gmail.com>
2023-08-22 13:53:25 +0100

    Add using to migration

--- db/migrate/20230818200130_change_player_names_to_json.rb
+++ db/migrate/20230818200130_change_player_names_to_json.rb
@@ -2,6 +2,6 @@
 
 class ChangePlayerNamesToJson < ActiveRecord::Migration[7.0]
   def change
-    change_column :players, :old_names, :json
+    change_column :players, :old_names, :json, using: 'old_names::json'
   end
 end
