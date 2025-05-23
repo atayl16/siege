8997b8f1828e1c1dd432ae0cd2e804a5434ec0ef
Alisha Taylor <inspirebyexample0@gmail.com>
2025-05-23 14:24:43 +0100

    Redirect to new site

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class WelcomeController < ApplicationController
-  def index; end
-
+  skip_before_action :verify_authenticity_token, only: [:redirect_notice]
+  skip_before_action :authenticate_user!, only: [:redirect_notice]
   def redirect_notice
     @new_site_url = "https://www.siege-clan.com"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # Only keep these two routes
   get 'redirect_notice', to: 'welcome#redirect_notice'
   get 'staying', to: 'application#staying'
-  resources :vars
-  devise_for :users
-  resources :players
-  resources :events
-  resources :achievements
-  get 'players/:id/delete', to: 'players#deactivate', as: 'delete_player'
-  get 'players/:id/activate', to: 'players#activate', as: 'activate_player'
-  get 'export', to: 'players#export', as: 'export'
-  get 'reset', to: 'players#reset', as: 'reset'
-  get 'reset_siege_scores', to: 'players#reset_siege_scores', as: 'reset_siege_scores'
-  get 'leaderboard', to: 'players#leaderboard', as: 'leaderboard'
-  get 'events/:id/delete', to: 'events#delete', as: 'delete_event'
-  get '/table', to: 'players#table', as: 'table'
-  get '/deleted', to: 'players#deleted', as: 'deleted'
-  get '/update_rank', to: 'players#update_rank', as: 'update_rank'
-  get '/switch_role', to: 'players#switch_role', as: 'switch_role'
-  get '/add_siege_score', to: 'players#add_siege_score', as: 'add_siege_score'
-  get 'welcome', to: 'welcome#index', as: 'welcome'
-  get '/gallery', to: 'events#gallery', as: 'gallery'
-  get 'logo_var', to: 'vars#logo_var', as: 'logo_var'
-  get 'hidden_players_var', to: 'vars#hidden_players_var', as: 'hidden_players_var'
-  get 'competition_state_var', to: 'vars#competition_state_var', as: 'competition_state_var'
-  get '/competition', to: 'players#competition', as: 'competition'
-  post 'whitelist_runewatch', to: 'players#whitelist_runewatch'
-  root 'welcome#index'
+  
+  # Redirect everything else to the redirect notice
+  match '*path', to: 'welcome#redirect_notice', via: :all
+  root 'welcome#redirect_notice'
 end
